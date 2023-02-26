693c40955b0ab3761909fc48cae5d3d69c465aa1
eliza galea <111540402+egalea504@users.noreply.github.com>
2023-02-25 23:35:01 -0500

    implemented event handler which displays server message

---
+++ play.js
@@ -0,0 +1,20 @@
+const net = require("net");
+
+const connect = function (data) {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541
+  });
+
+  conn.on("data", (data) => {
+    console.log("Game says: ", data);
+  });
+
+// interpret incoming data as text
+  conn.setEncoding("utf8");
+
+ return conn;
+};
+
+console.log("Connecting ...");
+connect();
