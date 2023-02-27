238f2b277eed50e41c0a523ed2666b4f15423ebf
eliza galea <111540402+egalea504@users.noreply.github.com>
2023-02-27 16:42:18 -0500

    implemented WASD movement

--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-const setupInput = function () {
+let connection;
+
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -8,9 +11,27 @@ const setupInput = function () {
 };
 
 const handleUserInput = (data) => {
+  // this tells server to move up when you press w
+  if (data === 'w') {
+    connection.write('Move: up');
+
+  // this tells server to move left when you press a
+  } else if (data === 'a') {
+    connection.write('Move: left');
+
+  // this tells server to move down when you press s
+  } else if (data === 's') {
+    connection.write('Move: down');
+
+  // this tells server to move right when you press d
+  } else if (data === 'd') {
+    connection.write('Move: right');
+
+  } else if
   // \u0003 maps to ctrl+c input
-if (data === '\u0003') {
+  (data === '\u0003') {
   process.exit();
+
 }};
 
 module.exports = {
--- play.js
+++ play.js
@@ -3,6 +3,5 @@ const { net , connect } = require("./client.js");
 const { setupInput } = require("./input");
 
 console.log("Connecting ...");
-connect();
-
-setupInput();
+//connect is passed in setupInput which will permit stdin to communicate with the server and pass the messages when pressing a key
+setupInput(connect());
