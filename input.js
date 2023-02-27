9fe5a4e2e5cb3798a793754a20484fbd60d1fa6e
eliza galea <111540402+egalea504@users.noreply.github.com>
2023-02-27 15:58:14 -0500

    setup function moved to input.js

---
+++ input.js
@@ -0,0 +1,18 @@
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = (data) => {
+  // \u0003 maps to ctrl+c input
+if (data === '\u0003') {
+  process.exit();
+}};
+
+module.exports = {
+  setupInput
+};
--- play.js
+++ play.js
@@ -1,22 +1,8 @@
 // const net = require("net");
 const { net , connect } = require("./client.js");
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
 connect();
 
-// setup interface to handle user input from stdin
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = () => {
-  // \u0003 maps to ctrl+c input
-if (key === '\u0003') {
-  process.exit();
-}};
+setupInput();
