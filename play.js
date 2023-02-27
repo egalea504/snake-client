f4344abc6dbdec681ce8964ba7d1e96308d1e119
eliza galea <111540402+egalea504@users.noreply.github.com>
2023-02-27 11:05:08 -0500

    setup stdin added

--- play.js
+++ play.js
@@ -3,3 +3,20 @@ const { net , connect } = require("./client.js");
 
 console.log("Connecting ...");
 connect();
+
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = () => {
+  // \u0003 maps to ctrl+c input
+if (key === '\u0003') {
+  process.exit();
+}};
